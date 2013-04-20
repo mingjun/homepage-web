27f0d3dad6ea3e63e2b0ada56f6a0dfdd868d089
Mingjun Xu <xmj0236@gmail.com>
2013-04-20 20:16:39 +0800

    update build script, for compressing html/css  Please enter the commit message for your changes. Lines starting

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 TARGET_DIR=${PWD}/../www
+CSS_COMPRESSOR=yuicompressor.jar
+HTML_COMPRESSOR=htmlcompressor.jar
+
 echo build target: $TARGET_DIR
 
 
@@ -10,8 +13,42 @@ cat <<LINES
 
 LINES
 
-echo synchronize html/img/css
+echo synchronize html/img
 rsync -rtpzuv --delete --exclude-from=exclude.list ./ $TARGET_DIR/
+
+cat <<LINES
+-------------------------------------------
+
+
+LINES
+
+echo compressing css ..
+rm -r $TARGET_DIR/css/*
+for FILE in css/*.css
+do
+    echo compress $FILE
+    java -jar $CSS_COMPRESSOR -o $TARGET_DIR"/"$FILE $FILE
+done
+
+cat <<LINES
+-------------------------------------------
+
+
+LINES
+
+echo compressing htmls ..
+
+function compressHTML {
+    echo compress $1
+    java -jar $HTML_COMPRESSOR --type html \
+	--compress-js --compress-css \
+	-o $TARGET_DIR/$1 $1
+}
+
+compressHTML index.html
+compressHTML wenxin/index.html
+compressHTML search/index.html
+
 
 cat <<LINES
 -------------------------------------------
